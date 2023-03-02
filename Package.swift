31069bd3c9eaea2c361220eac534b4f2916835cb
Yeonah <moltohvere@hanwha.com>
2023-03-02 11:49:02 +0900

    Support HDWalletKit SPM.

--- Package.swift
+++ Package.swift
@@ -10,21 +10,20 @@ let package = Package(
     ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
-        .library(
-            name: "HDWalletKit",
-            targets: ["HDWalletKit"]),
+        .library(name: "HDWalletKit", targets: ["HDWalletKit"]),
     ],
     dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        .package(url: "https://github.com/Boilertalk/secp256k1.swift", .upToNextMinor(from: "0.1.4")),
-        .package(url: "https://github.com/krzyzanowskim/CryptoSwift.git", .upToNextMajor(from: "1.0.0"))
+        // Dependencies ;declare other packages that this package depends on.
+        .package(url: "https://github.com/krzyzanowskim/CryptoSwift", from: "1.0.0"),
+        .package(url: "https://github.com/Boilertalk/secp256k1.swift.git", from: "0.1.4")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "HDWalletKit",
-            dependencies: []),
+            dependencies: ["CryptoSwift",
+                           .product(name: "secp256k1", package: "secp256k1.swift")]),
         .testTarget(
             name: "HDWalletKitTests",
             dependencies: ["HDWalletKit"]),
